fb881c0b01db658cab6939204bcea15484b1ceee
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-01-06 21:56:17 +0600

    push till 3 function creates

--- js-project(game)/script.js
+++ js-project(game)/script.js
@@ -24,6 +24,75 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 of the game starting 
 in below */
 
+//initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+
+    button1.innerText = "Buy 10 health (10 coins)";
+    button2.innerText = "Buy weapon (30 coins)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store";
+
+    // console.log("Going to store.")
+}
+function goCave() {
+    console.log("Going to cave.")
+}
+function fightDragon() {
+    console.log("Fighting with dragon.")
+}
+
+function buyHealth() {
+    
+}
+function buyWeapon() {
+
+}
+function goTown() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
